2c4c7e8f9d41fb7bec7da085cfa3679ab007e4db
Kenfus <vincenzo.timmel@gmail.com>
2021-03-09 18:35:11 +0100

    Added second line plot.

--- main.py
+++ main.py
@@ -5,12 +5,36 @@ import streamlit as st
 import pydeck as pdk
 import altair as alt
 import pathlib
+import time
 
 ### Param
 DATA_URL = "https://github.com/owid/covid-19-data/raw/master/public/data/owid-covid-data.csv"
 ISO_TO_LONLAT_URL = "https://gist.githubusercontent.com/cpl/3dc2d19137588d9ae202d67233715478/raw/3d801e76e1ec3e6bf93dd7a87b7f2ce8afb0d5de/countries_codes_and_coordinates.csv"
-LOCATION_COLUMN = 'location'
-THRESHOLD_FOR_CASES_PER_MILLION = 10000 # Some countries probably ofer no data. This is the threshold to filter those out. Also, to keep data lean, we drop a lot of countries.
+DATA_FOR_SELECTION = ['Total cases', 'New cases',
+       'New cases smoothed', 'Total deaths', 'New deaths',
+       'New deaths smoothed', 'Total cases per million',
+       'New cases per million', 'New cases smoothed per million',
+       'Total deaths per million', 'New deaths per million',
+       'New deaths smoothed per million', 'Reproduction rate', 'Icu patients',
+       'Icu patients per million', 'Hosp patients',
+       'Hosp patients per million', 'Weekly icu admissions',
+       'Weekly icu admissions per million', 'Weekly hosp admissions',
+       'Weekly hosp admissions per million', 'New tests', 'Total tests',
+       'Total tests per thousand', 'New tests per thousand',
+       'New tests smoothed', 'New tests smoothed per thousand',
+       'Positive rate', 'Tests per case', 'Tests units', 'Total vaccinations',
+       'People vaccinated', 'People fully vaccinated', 'New vaccinations',
+       'New vaccinations smoothed', 'Total vaccinations per hundred',
+       'People vaccinated per hundred', 'People fully vaccinated per hundred',
+       'New vaccinations smoothed per million', 'Stringency index',
+       'Population', 'Population density', 'Median age', 'Aged 65 older',
+       'Aged 70 older', 'Gdp per capita', 'Extreme poverty',
+       'Cardiovasc death rate', 'Diabetes prevalence', 'Female smokers',
+       'Male smokers', 'Handwashing facilities', 'Hospital beds per thousand',
+       'Life expectancy', 'Human development index']
+
+THRESHOLD_FOR_CASES_PER_MILLION = 10000 # Some countries probably offer no data or clearly wrong data. This is the threshold to filter those out. Also, to keep data lean, we drop a lot of countries.
+MAX_COUNTRIES_TO_SHOW = 30 
 
 # HACK This only works when we've installed streamlit with pipenv, so the
 # permissions during install are the same as the running process
@@ -28,49 +52,89 @@ def load_data():
     long_lat_map = pd.read_csv(ISO_TO_LONLAT_URL, engine='python', sep=',', skipinitialspace=True)[['Alpha-3 code','Latitude (average)', 'Longitude (average)']]
     long_lat_map = long_lat_map.rename(columns={"Latitude (average)": "lat", "Longitude (average)": "lon"})
     data = data.merge(long_lat_map, how = 'inner', left_on = 'iso_code', right_on = 'Alpha-3 code').drop(columns = 'iso_code')
+    data.columns = data.columns.str.replace("_", " ")
+    data.columns = map(str.capitalize, data.columns)
     data.to_csv(str(DOWNLOADS_PATH / "data.csv"), index=False)
 
 
+
+"""
+# Code Generator for Machine Learning
+[![Star](https://img.shields.io/github/stars/jrieke/traingenerator.svg?logo=github&style=social)](https://gitHub.com/jrieke/traingenerator/stargazers)
+&nbsp[![Follow](https://img.shields.io/twitter/follow/jrieke?style=social)](https://www.twitter.com/jrieke)
+&nbsp[![Buy me a coffee](https://img.shields.io/badge/Buy%20me%20a%20coffee--yellow.svg?logo=buy-me-a-coffee&logoColor=orange&style=social)](https://www.buymeacoffee.com/jrieke)
+"""
+st.markdown("<br>", unsafe_allow_html=True)
+"""Jumpstart your machine learning code:
+1. Specify model in the sidebar *(click on **>** if closed)*
+2. Training code will be generated below
+3. Download and do magic! :sparkles:
+---
+"""
+
 # Load data:
-data_load_state = st.text('Loading data')
-# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
 load_data()
-data = pd.read_csv(str(DOWNLOADS_PATH / "data.csv"), usecols = ['location', 'total_cases_per_million', 'total_deaths_per_million', 'total_tests_per_thousand']).drop_duplicates()
-data = data[data['total_cases_per_million']>THRESHOLD_FOR_CASES_PER_MILLION].dropna()
-data = data.pivot_table(index=['location'], values=['total_cases_per_million', 'total_deaths_per_million', 'total_tests_per_thousand'])
 data_load_state.text("Done!")
+time.sleep(1)
+data_load_state.text(None)
 
-# Show total cases per million per country:
-st.bar_chart(data)
+## Show total cases per million per country:
+# Load Data:
+data = pd.read_csv(str(DOWNLOADS_PATH / "data.csv"), usecols = ['Location', 'Total cases per million']).drop_duplicates()
+data = data[data['Total cases per million']>THRESHOLD_FOR_CASES_PER_MILLION].dropna()
+data = data.pivot_table(index=['Location'], values=['Total cases per million']).reset_index()
+data.sort_values(by=['Total cases per million'], inplace=True, ascending=False)
 
-# On a map:
-data = pd.read_csv(str(DOWNLOADS_PATH / "data.csv"), usecols = ['lon', 'lat', 'total_cases_per_million', 'total_deaths_per_million', 'total_tests_per_thousand']).drop_duplicates()
-data = data[data['total_cases_per_million']>THRESHOLD_FOR_CASES_PER_MILLION].dropna()
-data = data.pivot_table(index=['lon', 'lat'], values=['total_cases_per_million', 'total_deaths_per_million', 'total_tests_per_thousand']).reset_index()
-
-st.map(data)
-
-##
-st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
-    initial_view_state=pdk.ViewState(
-        latitude=42.8182,
-        longitude=9.2275,
-        zoom=2,
-        pitch=50
-    ),
-    layers=[
-        pdk.Layer(
-           'ColumnLayer',
-           data=data,
-           get_position='[lon, lat]',
-           radius=40000,
-           elevation_scale=10,
-           get_elevation="total_cases_per_million",
-           auto_highlight=True,
-           pickable=False,
-           extruded=True,
-        ),
-    ],
+# Plot Data:
+st.write("Top {} Countries with the highest number of total cases".format(MAX_COUNTRIES_TO_SHOW))
+st.write(alt.Chart(data.head(MAX_COUNTRIES_TO_SHOW)).mark_bar().encode(
+    x=alt.X('Location', sort=None),
+    y='Total cases per million',
 ))
 
+
+# Load countries in Data.
+countries_df = data['Location'].unique()
+
+# Show selectors for task and framework in sidebar (based on template_dict). These
+# selectors determine which template (from template_dict) is used (and also which
+# template-specific sidebar components are shown below).
+with st.sidebar:
+    st.info(
+        "🎈 Don't forget to give Vincenzo a high score!"
+    )
+    # st.error(
+    #     "Found a bug? [Report it](https://github.com/jrieke/traingenerator/issues) 🐛"
+    # )
+    st.write("## Task")
+    country = st.selectbox(
+        "Which Country do you want to analyse?", countries_df
+    )
+    if isinstance(country, str):
+        type_data = st.selectbox(
+            "What do you want to see?", list(DATA_FOR_SELECTION)
+        )
+        # Load Data:
+        data_selected = pd.read_csv(str(DOWNLOADS_PATH / "data.csv"), usecols = ['Date', 'Location', 'Total cases per million', type_data]).drop_duplicates()
+        data_selected = data_selected[data_selected['Total cases per million']>THRESHOLD_FOR_CASES_PER_MILLION].dropna()
+        #data_selected = data_selected.pivot_table(index=['Location'], values=[type_data]).reset_index()
+        data_selected['Date'] = pd.to_datetime(data_selected['Date'], format = '%Y-%m-%d')
+        data_selected = data_selected.query("Location == @country")
+        print(data_selected)
+    else:
+        pass
+
+# Plot selected Data:
+st.write("{} of {}.".format(type_data, country))
+
+#st.line_chart(data_selected)
+
+st.write(alt.Chart(data_selected.reset_index()).mark_area(
+    color="lightblue",
+    line=True
+    ).encode(
+        x='Date',
+        y=type_data
+        )
+)#.transform_filter(alt.datum.symbol == 'GOOG')
