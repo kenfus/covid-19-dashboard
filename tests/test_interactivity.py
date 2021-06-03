f92c445b5ea00a621e43a433dbfdd2a7fbc64b25
Vincenzo <vincenzo.timmel@gmail.com>
2021-06-03 12:43:30 +0200

    Readded lost tests.

--- tests/test_interactivity.py
+++ tests/test_interactivity.py
@@ -6,6 +6,26 @@ import time
 url = "http://localhost:8501"
 
 
+class Global_View(BaseCase):
+    def test_basics(self):
+        self.open(url)
+        self.assert_no_404_errors() 
+        self.assert_title("main · Streamlit")
+        self.assert_text("Our World in Data")
+        self.assert_text("Albania: Total cases.", timeout=30)
+        self.assert_text("New data fetched from https://github.com/owid!")
+        self.click("div.block-container:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > svg:nth-child(1)")
+        self.click_xpath("/html/body/div[1]/div[2]/div/div/div[3]/div/div/div/ul/div/div/li[7]/span")
+        self.assert_text("Global View: Total cases per million", timeout=30)
+        self.assert_element_present(".stPlotlyChart")
+
+
+class InternetAccess(BaseCase):
+    def test_basics(self):
+        self.open(url)
+        self.click("#explore-the-covid-data-from-our-world-in-data > div:nth-child(1) > span:nth-child(2) > a:nth-child(1)")
+        self.switch_to_window(1)
+        self.assert_title("Our World in Data · GitHub")
 
 
 class ScreenShotTest(BaseCase):
