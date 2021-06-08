ad5aad99fc157e8bdc865324a668860ce8337ded
Vincenzo <vincenzo.timmel@gmail.com>
2021-06-08 17:14:07 +0200

    Fixed test by using raw-version of the covid data.

--- tests/test_interactivity.py
+++ tests/test_interactivity.py
@@ -20,7 +20,7 @@ class Global_View(BaseCase):
 
 class CovidDataExist(BaseCase):
     def test_basics(self):
-        self.open("https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data.csv")
+        self.open("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
         self.assert_no_404_errors()
 
 class LongLatDataExist(BaseCase):
